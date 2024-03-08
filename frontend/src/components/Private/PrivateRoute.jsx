7475fcf6991c6cb0f503b3e57d34ec3de38c29ca
unique-surendra1 <154492995+unique-surendra1@users.noreply.github.com>
2024-03-08 18:49:45 +0530

    code modified

--- frontend/src/components/Private/PrivateRoute.jsx
+++ frontend/src/components/Private/PrivateRoute.jsx
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
   // console.log(userInfo);
-
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
